f4d366b7ba8c43531535981ab8d52e6c5647950d
tstef87 <65368155+tstef87@users.noreply.github.com>
2023-04-18 17:31:48 -0400

    tip and checkout

--- app/src/main/java/com/example/arcus/ui/register/RegisterMenuActivity.java
+++ app/src/main/java/com/example/arcus/ui/register/RegisterMenuActivity.java
@@ -3,7 +3,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.icu.text.DecimalFormat;
 import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -33,16 +36,15 @@ public class RegisterMenuActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    private ListView listView;
+    public ListView listView;
     //private ArrayAdapter <String> adapter;
 
-    private List<Sales> saleList;
-    private List <String> data;
-    private List<Double> priceList;
-    private TableRow tableRow;
+    public List<Sales> saleList;
+    public List <String> data;
+    public List<Double> priceList;
+    public TableRow tableRow;
 
-
-    private double sum = 0.00;
+    public double sum = 0.00;
 
 
 
@@ -51,14 +53,17 @@ public class RegisterMenuActivity extends AppCompatActivity {
         saleList = sales;
     }
 
-    private void addSale(@NonNull List<String> data, double sum, String id){
+    private void addSale(@NonNull List<String> data, double total, double s, double tax, double tip,  String id){
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
         ArrayList<String> items = new ArrayList<>(data);
 
         Map<String, Object> saleInfo = new HashMap<>();
-        saleInfo.put("Price", sum);
+        saleInfo.put("Subtotal", total);
+        saleInfo.put("Price", s);
+        saleInfo.put("Tax", tax);
+        saleInfo.put("Tip", tip);
         saleInfo.put("Items", items);
         saleInfo.put("Time", now.format(formatter));
         //saleInfo.put("Receipt", receipt);
@@ -71,11 +76,10 @@ public class RegisterMenuActivity extends AppCompatActivity {
         for (int i = 0; i < price.size(); i++){
             total += price.get(i);
         }
-        return total;
+        return fmt(total);
     }
 
     public void setPriceTV (TextView textView){
-
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
         textView.setText(numberFormat.format(sum));
     }
@@ -93,6 +97,15 @@ public class RegisterMenuActivity extends AppCompatActivity {
             }
         }
         return -1;
+    }
+
+    public double fmt(double num){
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        return Double.parseDouble(decimalFormat.format(num));
+    }
+
+    public double tipTaxCalc(double total, double times){
+        return fmt(total * times);
     }
 
 
@@ -148,10 +161,10 @@ public class RegisterMenuActivity extends AppCompatActivity {
                        Sales saleItem = saleList.get(index);
                        saleItem.setAmount(saleItem.getAmount() + 1);
                        data.set(index, saleItem.toString());
-                       priceList.set(index, priceList.get(index) + item.getPrice());
+                       priceList.set(index, priceList.get(index) + fmt(item.getPrice()));
 
                    }else {
-                       Sales sales = new Sales(item.getName(), 1, item.getPrice());
+                       Sales sales = new Sales(item.getName(), 1, fmt(item.getPrice()));
                        saleList.add(sales);
                        data.add(sales.toString());
                        priceList.add(sales.getPrice());
@@ -170,15 +183,7 @@ public class RegisterMenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(!data.isEmpty()) {
-                    showPopUp(view);
-//                    addSale(data, sum, id);
-//                    Item i = new Item();
-//                    sum = 0.00;
-//                    data.clear();
-//                    priceList.clear();
-//                    saleList.clear();
-//                    listView.setAdapter(listItemAdapter);
-//                    setPriceTV(price);
+                    tipScreen(view, tipTaxCalc(sum, .2), tipTaxCalc(sum, .18), tipTaxCalc(sum, .15), id, price, listItemAdapter);
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "No Items in List", Toast.LENGTH_SHORT).show();
@@ -211,14 +216,103 @@ public class RegisterMenuActivity extends AppCompatActivity {
         }
     }
 
-    public void showPopUp(View view) {
+
+    public void tipScreen(View view, double tip20, double tip18, double tip15, String id, TextView price, ListItemAdapter listItemAdapter) {
         // Create a new AlertDialog object and set its content to your pop-up layout
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.subtotal_popup, null);
+        View dialogView = inflater.inflate(R.layout.tip_prompt, null);
         builder.setView(dialogView);
         AlertDialog dialog = builder.create();
+
+        TextView textView20 = (TextView) dialogView.findViewById(R.id.tip20Text);
+        TextView textView18 = (TextView) dialogView.findViewById(R.id.tip18Text);
+        TextView textView15 = (TextView) dialogView.findViewById(R.id.tip15Text);
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        textView20.setText("$" + decimalFormat.format(tip20));
+        textView18.setText("$" + decimalFormat.format(tip18));
+        textView15.setText("$" + decimalFormat.format(tip15));
+
+        Button button0 = (Button) dialogView.findViewById(R.id.noTipButton);
+        Button button20 = (Button) dialogView.findViewById(R.id.tip20);
+        Button button18 = (Button) dialogView.findViewById(R.id.tip18);
+        Button button15 = (Button) dialogView.findViewById(R.id.tip15);
+
         dialog.show();
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                checkoutScreen(view.getContext(), 0.00, id, price, listItemAdapter);
+            }
+        });
+
+        button20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                checkoutScreen(view.getContext(), tip20, id, price, listItemAdapter);
+            }
+        });
+
+        button18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                checkoutScreen(view.getContext(), tip18, id, price, listItemAdapter);
+            }
+        });
+
+        button15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                checkoutScreen(view.getContext(), tip15, id, price, listItemAdapter);
+            }
+        });
     }
+    @SuppressLint("SetTextI18n")
+    public void checkoutScreen(Context context, double tip, String id, TextView price, ListItemAdapter listItemAdapter){
+        Dialog dialogCheckOut = new Dialog(context);
+        dialogCheckOut.setContentView(R.layout.checkout_prompt);
+        dialogCheckOut.show();
+
+        TextView grandTotal = dialogCheckOut.findViewById(R.id.grand_total);
+        TextView tipTotal = dialogCheckOut.findViewById(R.id.tip_total);
+        TextView tax = dialogCheckOut.findViewById(R.id.tax_total);
+
+        double taxSum = tipTaxCalc(sum, .06);
+        double total = fmt(sum + taxSum + tip);
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        grandTotal.setText("$" + decimalFormat.format(total));
+        tipTotal.setText("$" + decimalFormat.format(tip));
+        tax.setText("$" + decimalFormat.format(taxSum));
+
+
+        Button checkout = dialogCheckOut.findViewById(R.id.confirm_payment);
+        checkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogCheckOut.dismiss();
+                addSale(data, total, sum, taxSum, tip, id);
+                Item i = new Item();
+                sum = 0.00;
+                data.clear();
+                priceList.clear();
+                saleList.clear();
+                listView.setAdapter(listItemAdapter);
+                setPriceTV(price);
+            }
+        });
+
+
+
+    }
+
 }
